f1973232a5faf9c9820a7cd344551675f4095999
Ihedioha Ifeyinwa <ihediohaifeyinwa@gmail.com>
2023-07-09 22:53:18 +0100

    Initial commit

---
+++ src/BackgroundAnimate.js
@@ -0,0 +1,32 @@
+
+
+const BackgroundAnimate = () => {
+  return (
+    <ul class="background">
+        <div className="background">
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+</div>
+    </ul>
+  )
+}
+
+export default BackgroundAnimate
